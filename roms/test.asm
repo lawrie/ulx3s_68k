5537c9088d7cb95bbf02f56ed281d48e431baec3
lawrie <lawrie.griffiths@ntlworld.com>
2020-07-05 14:59:44 +0100

    fixed test

--- roms/test.asm
+++ roms/test.asm
@@ -1,15 +1,15 @@
 	ORG	$0000
 
-	DC.L	$20000	; Set stack to top of RAM
-	DC.L    start	; Set PC to start
+	DC.L	$20000		; Set stack to top of RAM
+	DC.L    start		; Set PC to start
 
 start:
 	MOVE.L	#$10000,A0	; VRAM address
-        MOVE.W  #$3FFF,D0	; Full screen in words
+	MOVE.W  #$3FFF,D0	; Full screen in words
 loop:
-	MOVE.W  #$6431, (A0)    ; Write to VRAM
-        ADD.L	#2, A0		; Increment VRAM pointer
-        DBRA    D0, loop	; Loop until done
+	MOVE.W  #$6431, (A0)	; Write to VRAM
+	ADDQ.L	#2, A0		; Increment VRAM pointer
+	DBRA    D0, loop	; Loop until done
 	STOP    #$2700
 
 
