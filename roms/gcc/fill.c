3d43d8d79c3eb7524637c82f29e3fafa3cab9e0e
emard <vordah@gmail.com>
2020-07-05 16:24:21 +0200

    fill.c update

--- roms/gcc/fill.c
+++ roms/gcc/fill.c
@@ -2,11 +2,10 @@ void main()
 {
   asm("dc.l 0x20000"); // Set stack to top of RAM
   asm("dc.l 8");       // Set PC to start
-  int *v = (int *) 0x10000; // video framebuffer
-  for(int i = 0; i < 256; i++)
+  short unsigned int *v = (short unsigned int *) 0x10000; // video framebuffer
+  for(short unsigned int i = 0; i < 256; i++)
   {
-    v[0] = i;
-    v[i] = i;
+    v[0] = v[i] = ((i&0xFF)<<8) | (i&0xFF);
     asm("nop");
   }
   stop: goto stop;
