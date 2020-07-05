c7a34f40cb0008f40d6cf7ab828fc5092c38d4f9
lawrie <lawrie.griffiths@ntlworld.com>
2020-07-05 10:54:19 +0100

    Not quite working video

--- roms/test.asm
+++ roms/test.asm
@@ -1,8 +1,12 @@
 	ORG	$0000
 
-	MOVE	D0,D0
-	;STOP    #$2700
-loop:
-	BRA	loop
+	DC.L	$20000	; Set stack to top of RAM
+	DC.L    start	; Set PC to start
+
+start:
+	LEA	$10000,A0
+	MOVE.W  #$6431, (A0)
+	STOP    #$2700
+
 
 
