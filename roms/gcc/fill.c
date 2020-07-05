7863fa08b87a16cc33994051599d27446673c418
emard <vordah@gmail.com>
2020-07-05 17:05:36 +0200

    gcc use linker script

--- roms/gcc/fill.c
+++ roms/gcc/fill.c
@@ -1,12 +1,17 @@
-void main()
+void fill(void)
+{
+  unsigned short int *v = (unsigned short int *) 0x10000; // video framebuffer
+  const short unsigned int N = 1024;
+  for(unsigned short int i = 0; i < N; i++)
+    v[i] = 0;
+  for(unsigned short int i = 0; i < N; i++)
+    v[i] = (i<<8) | (i&0xFF);
+  stop: goto stop;
+}
+
+void main(void)
 {
   asm("dc.l 0x20000"); // Set stack to top of RAM
-  asm("dc.l 8");       // Set PC to start
-  unsigned short int *v = (unsigned short int *) 0x10000; // video framebuffer
-  for(unsigned short int i = 0; i < 255; i++)
-  {
-    v[0] = v[i] = (i<<8) | i;
-    asm("nop");
-  }
-  stop: goto stop;
+  asm("dc.l 8");       // Set PC to execute fill()
+  fill();
 }
