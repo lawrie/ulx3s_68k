956635ccf29534602ed0e0763279762d855d0b05
emard <vordah@gmail.com>
2020-07-06 11:17:29 +0200

    sdram_cke = 1

--- src/test68.v
+++ src/test68.v
@@ -81,6 +81,7 @@ module test68
   wire clk_cpu   = clocks[1];
   wire clk_sdram = clocks[2];
   wire sdram_clk = clocks[3]; // phase shifted for chip
+  wire sdram_cke = 1'b1;
 
   // ===============================================================
   // Reset generation
